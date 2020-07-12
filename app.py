53ef7eac5bf13e47aa7776c80551eac740b3b150
koa <chaiwat15832120@gmail.com>
2020-07-12 15:52:34 +0700

    add all

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
 
+import postgresql_api
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app)
@@ -29,6 +31,13 @@ def my_sum():
 def mypage():
     username = request.args.get('name')
     return render_template('home.html', name=username)
+
+@app.route('/std_list')
+def std_list():
+    student_list = postgresql_api.get_student_data()
+    return render_template('table.html', student_list=student_list)
+
+
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=5000)
--- postgresql_api.py
+++ postgresql_api.py
@@ -2,7 +2,7 @@ import psycopg2
 import os
 try:
     # db_secret = os.environ["DATABASE_URL"]
-    db_secret = ''
+    db_secret = 'postgres://ieleliaubmehkq:d99b4a4efac7e97feba41df31312571f9d4975ce62e1eff5ba328eb8dfddb610@ec2-52-200-48-116.compute-1.amazonaws.com:5432/d2rb9fbc5vidu5'
     connection = psycopg2.connect(db_secret)
     connection.set_session(autocommit=True)
 
@@ -22,5 +22,18 @@ try:
 except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
 
-def get_example_data():
-    pass
+def get_student_data():
+    cur = connection.cursor()
+    cur.execute("""
+    SELECT firstname,lastname,age
+     FROM student;
+    """)
+    rows = cur.fetchall()
+    print('Student firstname:')
+    print(rows)
+    #for row in rows:
+     #   print("   ", row[0])
+    cur.close()
+    return rows
+
+get_student_data()
